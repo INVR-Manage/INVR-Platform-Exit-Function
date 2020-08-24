1f8b83b4a0a73037f90705e446bc90e0f3391966
invrpw <46528883+invrpw@users.noreply.github.com>
2020-08-24 16:01:29 +0200

    Exit Function for Unity Projects

--- ExitFunction.cs
+++ ExitFunction.cs
@@ -6,7 +6,8 @@ using UnityEngine.UI;
 
 public class ExitFunction : MonoBehaviour
 {
-    // HOW TO USE
+    
+	// HOW TO USE
     // 1. set script in hierachy (for example in an manager object). Make sure that it stays persistent.
     // 2. call "ExitFunction.Instance.ExitExperience();" at the place you want to quit the application.
     //   (i.e. by pressing a specific button or through the menu)
